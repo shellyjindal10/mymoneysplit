072b12ec10cca9b1f36991ef54d6b93f0959b7c3
shelly jindal <jinshelly@gmail.com>
2014-12-08 11:14:48 -0800

    for the modification code

--- lib/facebook/logout.php
+++ lib/facebook/logout.php
@@ -7,5 +7,5 @@ session_unset();
     $_SESSION['EMAIL'] =  NULL;
     $_SESSION['LOGOUT'] = NULL;
 $_SESSION['do_not_auto_login'] = true;
-header("Location: http://localhost/MoneySplit/php/LoginPage.php");        // you can enter home page here ( Eg : header("Location: " ."http://www.krizna.com"); 
+header("Location: http://mymoneysplit.com/mymoneysplit/php/LoginPage.php");        // you can enter home page here ( Eg : header("Location: " ."http://www.krizna.com"); 
 ?>
